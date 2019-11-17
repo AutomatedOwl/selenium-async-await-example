9c553dd24345926cbd39bf93e7577f72e97c0186
AutomatedOwl <automatedowl@yandex.com>
2019-11-17 17:43:25 +0200

    Update DriverBuilder.js

--- src/lib/DriverBuilder.js
+++ src/lib/DriverBuilder.js
@@ -5,7 +5,7 @@ export default class DriverBuilder {
   driver: WebDriverClass
 
   constructor () {
-    const builder = new webdriver.Builder().usingServer('http://zalenium.elminda.com/wd/hub')
+    const builder = new webdriver.Builder().usingServer('http://10.0.0.20:31112/wd/hub')
     // $FlowIssue getting chrome capabilities as method is a-ok
     const capabilities = webdriver.Capabilities['chrome']()
     builder.withCapabilities(capabilities)
